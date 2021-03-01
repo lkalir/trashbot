d1606b607ad4d5315b112bb32f41f50e35d7a9f4
Courtney Popielarz <cxp5606@rit.edu>
2021-03-01 00:43:37 -0500

    Fixed woof link

--- src/commands/stalker/memes.rs
+++ src/commands/stalker/memes.rs
@@ -7,7 +7,7 @@ pub fn is_meme(msg: &str) -> Option<String> {
   // Hash map of Memes
   let mut memes = HashMap::new();
   memes.insert("arf".to_string(),"https://www.youtube.com/watch?v=4o5baMYWdtQ".to_string());
-  memes.insert("woof".to_string(),"https://www.youtube.com/watch?v=w7x_lWJNnNg".to_string());
+  memes.insert("woof".to_string(),"https://www.youtube.com/watch?v=83m261lAlrs".to_string());
   memes.insert("mow".to_string(),"https://www.youtube.com/watch?v=w7x_lWJNnNg".to_string());
   memes.insert("money".to_string(),"https://www.youtube.com/watch?v=diVtzaZDP3o".to_string());
   memes.insert("shaw".to_string(),"https://www.reddit.com/r/HollowKnightMemes/comments/j45uwg/shaw/?utm_source=share&utm_medium=web2x&context=3".to_string());
