2276bf3f8de88d9cb64dc8819284a148b017875f
Liam Kalir <liamkalir9595@gmail.com>
2021-04-21 13:13:11 -0400

    then_some might not stabilize for a while, but then is now stable

--- src/commands/stalker/arch.rs
+++ src/commands/stalker/arch.rs
@@ -4,10 +4,8 @@
 static ARCH: &[&str] = &["arch", "btw"];
 
 /// Detects whether a message warrants an arch reaction
-///
-/// TODO: Rework to [bool::then_some] once that stabilizes
 pub fn is_arch(msg: &str) -> bool {
     ARCH.iter()
-        .find_map(|b| if msg.contains(b) { Some(()) } else { None })
+        .find_map(|b| msg.contains(b).then(|| ()))
         .is_some()
 }
--- src/commands/stalker/birbs.rs
+++ src/commands/stalker/birbs.rs
@@ -31,11 +31,9 @@ static BIRDS: &[&str] = &[
 ];
 
 /// Detects whether a message warrants a "bird up"
-///
-/// TODO: Rework to [bool::then_some] once that stabilizes
 pub fn is_bird(msg: &str) -> bool {
     BIRDS
         .iter()
-        .find_map(|b| if msg.contains(b) { Some(()) } else { None })
+        .find_map(|b| msg.contains(b).then(|| ()))
         .is_some()
 }
