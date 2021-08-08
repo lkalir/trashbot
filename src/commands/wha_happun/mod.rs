d3154457a2a83d973a21c2302c83ab23e4b76e42
Liam Kalir <liam.kalir@protonmail.com>
2021-08-08 12:25:46 -0400

    Updated depencies, made graphql TOO idiomatic

--- src/commands/wha_happun/mod.rs
+++ src/commands/wha_happun/mod.rs
@@ -1,6 +1,6 @@
 //! Summarize recent changes
 
-use graphql_client::*;
+use graphql_client::{GraphQLQuery, Response};
 use log::warn;
 use serde::Serialize;
 use serenity::{
@@ -33,7 +33,7 @@ pub async fn wha_happun(ctx: &Context, msg: &Message, args: Args) -> CommandResu
     let tok = env::var("GITHUB_TOKEN")?;
 
     let client = reqwest::Client::builder()
-        .user_agent("graphql-rust/0.9.0")
+        .user_agent("graphql-rust/0.10.0")
         .build()?;
     let res = client
         .post("https://api.github.com/graphql")
@@ -45,41 +45,42 @@ pub async fn wha_happun(ctx: &Context, msg: &Message, args: Args) -> CommandResu
     res.error_for_status_ref()?;
 
     let body: Response<commit_query::ResponseData> = res.json().await?;
-    let mut found = false;
 
-    // Is this idiomatic rust's true power?
-    if let Some(data) = body.data {
-        if let Some(repo) = data.repository {
-            if let Some(obj) = repo.object {
-                if let commit_query::CommitQueryRepositoryObjectOn::Commit(com) = obj.on {
-                    if let Some(nodes) = com.history.nodes {
-                        // Build list of commits
-                        let mut updates: Vec<SmolStr> = nodes
-                            .iter()
-                            .filter_map(|node| node.as_ref())
-                            .map(|node| {
-                                format!("* {} - {}", node.abbreviated_oid, node.message_headline)
-                                    .into()
-                            })
-                            .collect();
+    // THIS is the power of idiomatic rust. Fear its destructuring power!
+    if let Response::<commit_query::ResponseData> {
+        data:
+            Some(commit_query::ResponseData {
+                repository:
+                    Some(commit_query::CommitQueryRepository {
+                        object:
+                            Some(commit_query::CommitQueryRepositoryObject::Commit(
+                                commit_query::CommitQueryRepositoryObjectOnCommit {
+                                    history:
+                                        commit_query::CommitQueryRepositoryObjectOnCommitHistory {
+                                            nodes: Some(nodes),
+                                        },
+                                },
+                            )),
+                    }),
+            }),
+        ..
+    } = body
+    {
+        // Build list of commits
+        let mut updates: Vec<SmolStr> = nodes
+            .iter()
+            .filter_map(|node| node.as_ref())
+            .map(|node| format!("* {} - {}", node.abbreviated_oid, node.message_headline).into())
+            .collect();
 
-                        // Insert some discord formatting
-                        updates.insert(0, "```".into());
-                        updates.insert(0, "Here's what I learned to do recently!".into());
-                        updates.push("```".into());
-                        let update_msg = updates.join("\n");
+        // Insert some discord formatting
+        updates.insert(0, "```".into());
+        updates.insert(0, "Here's what I learned to do recently!".into());
+        updates.push("```".into());
+        let update_msg = updates.join("\n");
 
-                        msg.channel_id.say(&ctx.http, update_msg).await?;
-
-                        // Set flag to indicate we actually succeeded
-                        found = true;
-                    }
-                }
-            }
-        }
-    }
-
-    if !found {
+        msg.channel_id.say(&ctx.http, update_msg).await?;
+    } else {
         warn!("Missing data from query");
         return Err("Missing data from query".into());
     }
